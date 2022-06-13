f5631e9c1005e5b82a731e7f9b185a762503fd91
Egor Koldasov <koldasov3@gmail.com>
2022-06-13 15:40:38 +0300

    Add title.

--- src/components/PersonalPage/PersonalPage.tsx
+++ src/components/PersonalPage/PersonalPage.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { FullScreenBox } from "./FullScreenBox";
 import { Section1 } from "./Section1";
 import { Section2 } from './Section2';
@@ -7,6 +8,9 @@ import { Section3 } from "./Section3";
 export const PersonalPage = () => {
   return (
     <FullScreenBox>
+      <Head>
+        <title>Egor Koldasov. Web Developer. React, Typescript, SQL, Node.</title>
+      </Head>
       <Section1 />
       <Section2 />
       <Section3 />
